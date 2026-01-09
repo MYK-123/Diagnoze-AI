e7fbb0551479d264a25c7077d88930cc7d804770
M Y K <m.yusufkml@gmail.com>
2026-01-09 22:59:17 +0530

    Added Some Core Function

--- core/__init__.py
+++ core/__init__.py
@@ -5,7 +5,7 @@ from auth.users import User
 from content.educational_content import EducationContent, get_all_education_content_cache
 from core.disease import Disease, Diseases
 from core.disease_symptoms_relation import load_all_relations
-from core.symptoms import Symptom, Symptoms
+from core.symptoms import Symptom, Symptoms, get_all_symptoms_cache
 from core.symptoms_input import SymptomInputs, add_new_symptom_input
 
 
@@ -17,6 +17,28 @@ def get_disease_list_from_symptom_list(symptom_list: Symptoms|list[Symptom]) -> 
     # relationship = get_all_diseases_symptom_relation().get_all_content_list()
 
     return []
+
+def get_disease_list_from_symptom_list_incude_exclude(include_list: Symptoms|list[Symptom], exclude_list: Symptoms|list[Symptom]) -> list[Disease]:
+    """
+    If include_list in empty returns []
+    """
+    A = get_disease_list_from_symptom_list(include_list)
+    if not A:
+        return []
+    B = get_disease_list_from_symptom_list(include_list)
+    return A if not B else list(set(A) - set(B))
+
+def get_top_bottom_k_diseases_from_diseases_list(diseases: list[Disease], k: int) -> tuple[list[Disease], list[Disease]]:
+    """
+    Return the top and bottom K diseases ranked by severity level.
+    
+    Severity ranking: high > medium > low.
+    """
+    top_k = get_top_k_diseases_from_diseases_list(diseases, k)
+    bottom_k = get_bottom_k_diseases_from_diseases_list(diseases, k)
+    return top_k, bottom_k
+
+
 
 def get_top_k_diseases_from_diseases_list(diseases: list[Disease], k: int) -> list[Disease]:
     """
@@ -30,6 +52,18 @@ def get_top_k_diseases_from_diseases_list(diseases: list[Disease], k: int) -> li
     
     return heapq.nlargest(k, diseases, key=lambda d: d.get_severity_level())
 
+def get_bottom_k_diseases_from_diseases_list(diseases: list[Disease], k: int) -> list[Disease]:
+    """
+    Return the bottom K diseases ranked by severity level.
+    
+    Severity ranking: high > medium > low.
+    """
+    
+    if not diseases or k <= 0:
+        return []
+    
+    return heapq.nsmallest(k, diseases, key=lambda d: d.get_severity_level())
+
 def get_education_content_for_diseases(diseases : Diseases | list[Disease]) -> list[EducationContent]:
     """	
     Given a Diseases object or a list of Disease objects,
@@ -42,6 +76,10 @@ def get_education_content_for_diseases(diseases : Diseases | list[Disease]) -> l
     disease_ids = {d.get_disease_id() for d in diseases_list}
     return [content for content in all_content if content.get_disease_id() in disease_ids]
 
+
+
+
+# TODO: Later using T5-base or T5-small or FLAN-T5, and train via nltk or spacy
 
 def symptom_list_from_input(from_input :str, user: User) -> SymptomInputs:
     # use NLP to identify list of symptoms from from_input
